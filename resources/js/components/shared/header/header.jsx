e01002dd6b9e741335934600e6605e7868e9451f
sansay500 <borodachev@gmail.com>
2022-07-27 10:46:20 +0300

    Google script

--- resources/js/components/shared/header/header.jsx
+++ resources/js/components/shared/header/header.jsx
@@ -1,18 +1,27 @@
 import GoMenu from "@/components/shared/header/goMenu/goMenu";
-import { Link } from "@inertiajs/inertia-react";
+import {Link} from "@inertiajs/inertia-react";
 import style from "./header.module.css";
+import {Helmet} from "react-helmet";
 
-const Header = ({ styleBtn }) => {
+
+const Header = ({styleBtn}) => {
     return (
-        <header className={`${style.header}`}>
-            <div>
-                <Link href="/" className={`${style.headerLogo}`}>
-                    <img src="/images/logo-upwork.svg" alt="Logo"></img>
-                </Link>
-            </div>
+        <>
+            <Helmet>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-232234708-1"/>
+            </Helmet>
+            <header className={`${style.header}`}>
 
-            <GoMenu styleBtn={styleBtn} />
-        </header>
+                <div>
+                    PRIVET
+                    <Link href="/" className={`${style.headerLogo}`}>
+                        <img src="/images/logo-upwork.svg" alt="Logo"></img>
+                    </Link>
+                </div>
+
+                <GoMenu styleBtn={styleBtn}/>
+            </header>
+        </>
     );
 };
 export default Header;
